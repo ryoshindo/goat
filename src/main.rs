fcf3debe1eb06479e372f0a77189e87ffa92cdd0
ryoshindo <udoryo999@gmail.com>
2024-01-20 19:36:36 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,39 @@
 use aws_sdk_iam::error::SdkError;
-use aws_sdk_iam::operation::list_access_keys::{ListAccessKeysError, ListAccessKeysOutput};
-use aws_sdk_iam::operation::list_users::{ListUsersError, ListUsersOutput};
+use aws_sdk_iam::operation::list_access_keys::ListAccessKeysError;
+use aws_sdk_iam::operation::list_users::ListUsersError;
+use aws_sdk_iam::types::{AccessKeyMetadata, User};
 use aws_sdk_iam::Client;
 
-async fn list_users(client: &Client) -> Result<ListUsersOutput, SdkError<ListUsersError>> {
+async fn list_users(client: &Client) -> Result<Vec<User>, SdkError<ListUsersError>> {
     let response = client.list_users().send().await?;
 
-    Ok(response)
+    Ok(response.users)
 }
 
 async fn list_access_keys_per_user(
     client: &Client,
     user_name: &str,
-) -> Result<ListAccessKeysOutput, SdkError<ListAccessKeysError>> {
+) -> Result<Vec<AccessKeyMetadata>, SdkError<ListAccessKeysError>> {
     let response = client
         .list_access_keys()
         .user_name(user_name)
         .send()
         .await?;
 
-    for access_key_metadata in response.clone().access_key_metadata {
-        println!(
-            "{}: {}",
-            user_name,
-            access_key_metadata.access_key_id.unwrap()
-        );
+    Ok(response.access_key_metadata)
+}
+
+async fn list_access_keys(
+    client: &Client,
+    users: Vec<User>,
+) -> Result<Vec<AccessKeyMetadata>, SdkError<ListAccessKeysError>> {
+    let mut access_keys = Vec::new();
+    for user in users {
+        let response = list_access_keys_per_user(client, &user.user_name).await?;
+        access_keys.extend(response);
     }
 
-    Ok(response)
+    Ok(access_keys)
 }
 
 #[tokio::main]
@@ -36,9 +42,11 @@ async fn main() {
     let client = Client::new(&shared_config);
 
     let users = list_users(&client).await.expect("failed to list users");
-    for user in users.users {
-        list_access_keys_per_user(&client, &user.user_name)
-            .await
-            .expect("failed to list access keys");
+    let access_keys = list_access_keys(&client, users)
+        .await
+        .expect("failed to list access keys");
+
+    for access_key in access_keys {
+        println!("access_key_id: {}", access_key.access_key_id.unwrap());
     }
 }
