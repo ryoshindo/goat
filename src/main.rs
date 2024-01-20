60b3b3513ab5e1d82b2ad2b60044b510ebcc7ae8
ryoshindo <udoryo999@gmail.com>
2024-01-20 16:41:26 +0900

    feat: list access key

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,44 @@
-use aws_sdk_dynamodb::{Client, Error};
+use aws_sdk_iam::error::SdkError;
+use aws_sdk_iam::operation::list_access_keys::{ListAccessKeysError, ListAccessKeysOutput};
+use aws_sdk_iam::operation::list_users::{ListUsersError, ListUsersOutput};
+use aws_sdk_iam::Client;
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    let shared_config = aws_config::load_from_env().await;
-    let client = Client::new(&shared_config);
+async fn list_users(client: &Client) -> Result<ListUsersOutput, SdkError<ListUsersError>> {
+    let response = client.list_users().send().await?;
 
-    let resp = client
-        .list_tables()
-        .limit(10)
+    Ok(response)
+}
+
+async fn list_access_keys_per_user(
+    client: &Client,
+    user_name: &str,
+) -> Result<ListAccessKeysOutput, SdkError<ListAccessKeysError>> {
+    let response = client
+        .list_access_keys()
+        .user_name(user_name)
         .send()
         .await?;
 
-    println!("Tables: {:?}", resp.table_names.unwrap());
+    for access_key_metadata in response.clone().access_key_metadata {
+        println!(
+            "{}: {}",
+            user_name,
+            access_key_metadata.access_key_id.unwrap()
+        );
+    }
 
-    Ok(())
+    Ok(response)
+}
+
+#[tokio::main]
+async fn main() {
+    let shared_config = aws_config::load_from_env().await;
+    let client = Client::new(&shared_config);
+
+    let users = list_users(&client).await.expect("failed to list users");
+    for user in users.users {
+        list_access_keys_per_user(&client, &user.user_name)
+            .await
+            .expect("failed to list access keys");
+    }
 }
