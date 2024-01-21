0134b873bb5c9989017f61f49daa3ad39d6dc329
ryoshindo <udoryo999@gmail.com>
2024-01-21 13:50:46 +0900

    feat: introduce struct

--- src/main.rs
+++ src/main.rs
@@ -4,47 +4,58 @@ use aws_sdk_iam::operation::list_users::ListUsersError;
 use aws_sdk_iam::types::{AccessKeyMetadata, User};
 use aws_sdk_iam::Client;
 
-async fn list_users(client: &Client) -> Result<Vec<User>, SdkError<ListUsersError>> {
-    let response = client.list_users().send().await?;
-
-    Ok(response.users)
+struct Command {
+    client: Client,
 }
 
-async fn list_access_keys_per_user(
-    client: &Client,
-    user_name: &str,
-) -> Result<Vec<AccessKeyMetadata>, SdkError<ListAccessKeysError>> {
-    let response = client
-        .list_access_keys()
-        .user_name(user_name)
-        .send()
-        .await?;
-
-    Ok(response.access_key_metadata)
-}
-
-async fn list_access_keys(
-    client: &Client,
-    users: Vec<User>,
-) -> Result<Vec<AccessKeyMetadata>, SdkError<ListAccessKeysError>> {
-    let mut access_keys = Vec::new();
-    for user in users {
-        let response = list_access_keys_per_user(client, &user.user_name).await?;
-        access_keys.extend(response);
+impl Command {
+    fn new(client: Client) -> Self {
+        Self { client }
     }
 
-    Ok(access_keys)
+    async fn list_users(&self) -> Result<Vec<User>, SdkError<ListUsersError>> {
+        let response = self.client.list_users().send().await?;
+
+        Ok(response.users)
+    }
+
+    async fn list_access_keys_per_user(
+        &self,
+        user_name: &str,
+    ) -> Result<Vec<AccessKeyMetadata>, SdkError<ListAccessKeysError>> {
+        let response = self
+            .client
+            .list_access_keys()
+            .user_name(user_name)
+            .send()
+            .await?;
+
+        Ok(response.access_key_metadata)
+    }
+
+    async fn list_access_keys(
+        &self,
+    ) -> Result<Vec<AccessKeyMetadata>, SdkError<ListAccessKeysError>> {
+        let mut access_keys = Vec::new();
+        for user in self.list_users().await.unwrap() {
+            let mut access_keys_per_user = self
+                .list_access_keys_per_user(&user.user_name)
+                .await
+                .unwrap();
+            access_keys.append(&mut access_keys_per_user);
+        }
+
+        Ok(access_keys)
+    }
 }
 
 #[tokio::main]
 async fn main() {
     let shared_config = aws_config::load_from_env().await;
     let client = Client::new(&shared_config);
+    let command = Command::new(client);
 
-    let users = list_users(&client).await.expect("failed to list users");
-    let access_keys = list_access_keys(&client, users)
-        .await
-        .expect("failed to list access keys");
+    let access_keys = command.list_access_keys().await.unwrap();
 
     for access_key in access_keys {
         println!("access_key_id: {}", access_key.access_key_id.unwrap());
